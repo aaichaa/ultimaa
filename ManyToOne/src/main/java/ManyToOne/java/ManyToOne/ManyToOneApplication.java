18074dc716d319822e8537248dea5d56516231cd
Aissatou Bobo <abd2004bobo@gmail.com>
2024-06-23 21:35:04 +0200

    feat config monitoring

--- ManyToOne/src/main/java/ManyToOne/java/ManyToOne/ManyToOneApplication.java
+++ ManyToOne/src/main/java/ManyToOne/java/ManyToOne/ManyToOneApplication.java
@@ -1,13 +1,21 @@
 package ManyToOne.java.ManyToOne;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableAdminServer
 public class ManyToOneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManyToOneApplication.class, args);
 	}
 
+	@Bean
+	ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
